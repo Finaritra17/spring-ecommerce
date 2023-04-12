47a9b439ac82581b0f01fcacb9569bfe2452980c
Finaritra17 <finaritrarandrianarivelo17@gmail.com>
2023-04-12 13:48:24 +0200

    implementar las funcionalidades de cargar imagen y mostrar imagen para el administrador

--- src/main/java/com/curso/ecommerce/controller/ProductoController.java
+++ src/main/java/com/curso/ecommerce/controller/ProductoController.java
@@ -52,19 +52,11 @@ public class ProductoController {
 		
 		//imagen
 		if(producto.getId()==null) { //cuando se crea un producto
-			String nombreImagen=upload.saveImage(null);
+			String nombreImagen=upload.saveImage(file);
 			producto.setImagen(nombreImagen);
 		}
 		else {
-			if(file.isEmpty()) {
-				Producto p=new Producto();
-				p=productoService.get(producto.getId()).get();
-				producto.setImagen(p.getImagen());
-			}
-			else {
-				String nombreImagen=upload.saveImage(file);
-				producto.setImagen(nombreImagen);
-			}
+				
 		}
 		
 		productoService.save(producto);
@@ -84,8 +76,27 @@ public class ProductoController {
 	}
 	
 	@PostMapping("/update")
-	public String update(Producto producto) {
+	public String update(Producto producto, @RequestParam("img") MultipartFile file) throws IOException {
 		
+		Producto p=new Producto();
+		p=productoService.get(producto.getId()).get();
+		
+		if(file.isEmpty()) {
+			producto.setImagen(p.getImagen());
+		}
+		else {// cuando se edita tbn la imagen			
+			//eliminar cuando no sea la imagen por defecto
+			
+			if(!p.getImagen().equals("default.jpg")) {
+				upload.deleteImage(p.getImagen());
+			}
+			
+			String nombreImagen=upload.saveImage(file);
+			producto.setImagen(nombreImagen);
+			
+		}
+		
+		producto.setUsuario(p.getUsuario());
 		productoService.update(producto);
 		return "redirect:/productos";
 				
@@ -93,6 +104,14 @@ public class ProductoController {
 	
 	@GetMapping("/delete/{id}")
 	public String delete(@PathVariable Integer id) {
+		
+		Producto p = new Producto();
+		p=productoService.get(id).get();
+		
+		//eliminar cuando no sea la imagen por defecto
+		if(!p.getImagen().equals("default.jpg")) {
+			upload.deleteImage(p.getImagen());
+		}
 		
 		productoService.delete(id);
 		return "redirect:/productos";
